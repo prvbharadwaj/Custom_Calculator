4e2055924c5925f52659d6f81605732573ae4fa0
prvbharadwaj <prvbharadwaj@gmail.com>
2020-04-22 22:16:19 +0530

    Added the following features: * All Clear (AC) * Clear (C) * Handling decimal point

--- calculator/src/components/Keypad/Keypad.js
+++ calculator/src/components/Keypad/Keypad.js
@@ -7,27 +7,73 @@ class Keypad extends Component {
     constructor(props, ...rest) {
         super(props, ...rest);
         this.state = {
-            keyboard_input : 0
+            keyboard_input : 0,
+            decimal_flag : false
         };
     }
 
     handleNumberInput = (num) =>{
-        let temp = this.state.keyboard_input*10 + num
-        this.setState({
-            keyboard_input : temp
-        });
-        console.log(this.state.keyboard_input)
+        if(this.state.decimal_flag)
+        {
+            if(Number.isInteger(this.state.keyboard_input))
+            {
+                this.setState({
+                    keyboard_input : this.state.keyboard_input + num/10
+                })
+            }
+            else
+            {
+                let temp = parseInt(this.state.keyboard_input.toString().split(".")[1])
+                let temp2 = temp*10 + num
+                let len = temp2.toString().length
+                this.setState({
+                    keyboard_input : parseInt(this.state.keyboard_input) + temp2/(Math.pow(10,len))
+                })
+            }
+        }
+        else if(!this.state.decimal_flag)
+        {
+            this.setState({
+                keyboard_input : this.state.keyboard_input*10 + num
+            });
+        }
+    }
+
+    handleDecimalInput = () =>{
+        this.setState({decimal_flag : true})
     }
 
     handleClearInput = (code) =>{
         if(code === 1)
-            this.setState({keyboard_input : 0})
+            this.setState({keyboard_input : 0, decimal_flag : false})
+        else if(code === 2)
+        {
+            if(this.state.decimal_flag && !Number.isInteger(this.state.keyboard_input))
+            {
+                let temp = parseInt(this.state.keyboard_input.toString().split(".")[1])
+                let len = parseInt(temp/10).toString().length
+                let new_dec_part = (parseInt(temp/10))/(Math.pow(10, len))
+                if(new_dec_part === 0)
+                {
+                    this.setState({decimal_flag : false})
+                }                    
+                this.setState({keyboard_input : parseInt(this.state.keyboard_input) + new_dec_part})           
+            }
+            else if (this.state.decimal_flag && Number.isInteger(this.state.keyboard_input))
+            {
+                this.setState({decimal_flag : false})
+            }
+            else if(!this.state.decimal_flag)
+            {
+                this.setState({keyboard_input : Math.floor(this.state.keyboard_input/10)})
+            }
+        }
     }
 
 
     render() {
         return (
-            <div className='keypad-master'>                
+            <div className='keypad-master'>                   
                 <div className='keypad-container'>
                     <div className='actual-keypad'>
                         <div className='func-rows'>
@@ -56,7 +102,7 @@ class Keypad extends Component {
                         </div>
                         <div className='num-rows'>
                             <p id='double-size' onClick={e => this.handleNumberInput(0)}>0</p>
-                            <p>.</p>
+                            <p onClick={e => this.handleDecimalInput()}>.</p>
                             <p className='four-or' style={{fontSize:'19px'}}>=</p>
                         </div>
                     </div>       

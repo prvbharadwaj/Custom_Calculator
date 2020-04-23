0c718f5e6c63e20ff0ea41c36d945989f9ebbaef
prvbharadwaj <prvbharadwaj@gmail.com>
2020-04-23 23:30:59 +0530

    Added the following features: * Added two states to accept two numbers for operations * Ability to register the various operations, operator flag state * Added operator clear (OC) functionality * Added two decimal flags, one for each state holding numbers

--- calculator/src/components/Keypad/Keypad.js
+++ calculator/src/components/Keypad/Keypad.js
@@ -8,66 +8,200 @@ class Keypad extends Component {
         super(props, ...rest);
         this.state = {
             keyboard_input : 0,
-            decimal_flag : false
+            keyboard_input2 : 0,
+            operator_flag : false,
+            operation_object : {},
+            decimal_flag1 : false,
+            decimal_flag2 : false
         };
     }
 
+
+
     handleNumberInput = (num) =>{
-        if(this.state.decimal_flag)
+        if(!this.state.operator_flag) //holds 1st number
         {
-            if(Number.isInteger(this.state.keyboard_input))
+            if(this.state.decimal_flag1)
             {
-                this.setState({
-                    keyboard_input : this.state.keyboard_input + num/10
-                })
+                if(Number.isInteger(this.state.keyboard_input))
+                {
+                    this.setState({
+                        keyboard_input : this.state.keyboard_input + num/10
+                    })
+                }
+                else
+                {
+                    let temp = parseInt(this.state.keyboard_input.toString().split(".")[1])
+                    let temp2 = temp*10 + num
+                    let len = temp2.toString().length
+                    this.setState({
+                        keyboard_input : parseInt(this.state.keyboard_input) + temp2/(Math.pow(10,len))
+                    })
+                }
             }
-            else
+            else if(!this.state.decimal_flag1)
             {
-                let temp = parseInt(this.state.keyboard_input.toString().split(".")[1])
-                let temp2 = temp*10 + num
-                let len = temp2.toString().length
                 this.setState({
-                    keyboard_input : parseInt(this.state.keyboard_input) + temp2/(Math.pow(10,len))
-                })
+                    keyboard_input : this.state.keyboard_input*10 + num
+                });
             }
         }
-        else if(!this.state.decimal_flag)
+        else //holds 2nd number
         {
-            this.setState({
-                keyboard_input : this.state.keyboard_input*10 + num
-            });
+            if(this.state.decimal_flag2)
+            {
+                if(Number.isInteger(this.state.keyboard_input2))
+                {
+                    this.setState({
+                        keyboard_input2 : this.state.keyboard_input2 + num/10
+                    })
+                }
+                else
+                {
+                    let temp = parseInt(this.state.keyboard_input2.toString().split(".")[1])
+                    let temp2 = temp*10 + num
+                    let len = temp2.toString().length
+                    this.setState({
+                        keyboard_input2 : parseInt(this.state.keyboard_input2) + temp2/(Math.pow(10,len))
+                    })
+                }
+            }
+            else if(!this.state.decimal_flag2)
+            {
+                this.setState({
+                    keyboard_input2 : this.state.keyboard_input2*10 + num
+                });
+            }
         }
     }
+
+
 
     handleDecimalInput = () =>{
-        this.setState({decimal_flag : true})
+        if(!this.state.operator_flag)
+        {
+            this.setState({decimal_flag1 : true})
+        }
+        else
+            this.setState({decimal_flag2 : true})
     }
 
+
+
     handleClearInput = (code) =>{
-        if(code === 1)
-            this.setState({keyboard_input : 0, decimal_flag : false})
-        else if(code === 2)
+        if(!this.state.operator_flag) //clear for 1st number
         {
-            if(this.state.decimal_flag && !Number.isInteger(this.state.keyboard_input))
+            if(code === 1)
+                this.setState({keyboard_input : 0, decimal_flag1 : false})
+            else if(code === 2)
             {
-                let temp = parseInt(this.state.keyboard_input.toString().split(".")[1])
-                let len = parseInt(temp/10).toString().length
-                let new_dec_part = (parseInt(temp/10))/(Math.pow(10, len))
-                if(new_dec_part === 0)
+                if(this.state.decimal_flag1 && !Number.isInteger(this.state.keyboard_input))
                 {
-                    this.setState({decimal_flag : false})
-                }                    
-                this.setState({keyboard_input : parseInt(this.state.keyboard_input) + new_dec_part})           
-            }
-            else if (this.state.decimal_flag && Number.isInteger(this.state.keyboard_input))
-            {
-                this.setState({decimal_flag : false})
-            }
-            else if(!this.state.decimal_flag)
-            {
-                this.setState({keyboard_input : Math.floor(this.state.keyboard_input/10)})
+                    let temp = parseInt(this.state.keyboard_input.toString().split(".")[1])
+                    let len = parseInt(temp/10).toString().length
+                    let new_dec_part = (parseInt(temp/10))/(Math.pow(10, len))
+                    if(new_dec_part === 0)
+                    {
+                        this.setState({decimal_flag1 : false})
+                    }                    
+                    this.setState({keyboard_input : parseInt(this.state.keyboard_input) + new_dec_part})           
+                }
+                else if (this.state.decimal_flag1 && Number.isInteger(this.state.keyboard_input))
+                {
+                    this.setState({decimal_flag1 : false})
+                }
+                else if(!this.state.decimal_flag1)
+                {
+                    this.setState({keyboard_input : Math.floor(this.state.keyboard_input/10)})
+                }
             }
         }
+        else //clear for 2nd number
+        {
+             if(code === 1)
+                this.setState({keyboard_input2 : 0, decimal_flag2 : false})
+            else if(code === 2)
+            {
+                if(this.state.decimal_flag2 && !Number.isInteger(this.state.keyboard_input2))
+                {
+                    let temp = parseInt(this.state.keyboard_input2.toString().split(".")[1])
+                    let len = parseInt(temp/10).toString().length
+                    let new_dec_part = (parseInt(temp/10))/(Math.pow(10, len))
+                    if(new_dec_part === 0)
+                    {
+                        this.setState({decimal_flag2 : false})
+                    }                    
+                    this.setState({keyboard_input2 : parseInt(this.state.keyboard_input2) + new_dec_part})           
+                }
+                else if (this.state.decimal_flag2 && Number.isInteger(this.state.keyboard_input2))
+                {
+                    this.setState({decimal_flag2 : false})
+                }
+                else if(!this.state.decimal_flag2)
+                {
+                    this.setState({keyboard_input2 : Math.floor(this.state.keyboard_input2/10)})
+                }                
+            }
+        }
+    }
+
+
+
+    handleOperatorClear = () =>{
+        this.setState({operator_flag : false})
+    }
+    
+
+
+
+    handleOperatorInput = (code) =>{
+        // reject operator input if operator flag is true (indicates that there is already an existing operation)
+        let a = {}
+        switch(code){
+            case 1: 
+                Object.assign(a, {div : true})
+                this.setState({
+                    operator_flag : true,
+                    operation_object : a
+                })
+                break;
+
+            case 2:
+                Object.assign(a, {mul : true})
+                this.setState({
+                    operator_flag : true,
+                    operation_object : a
+                })
+                break;
+            
+            case 3:
+                Object.assign(a, {add : true})
+                this.setState({
+                    operator_flag : true,
+                    operation_object : a
+                })
+                break;
+            
+            case 4:
+                Object.assign(a, {sub : true})
+                this.setState({
+                    operator_flag : true,
+                    operation_object : a
+                })
+                break;
+
+            default:
+                this.setState({
+                    operator_flag : false,
+                    operation_object : {}
+                })
+                break;
+        }
+    }
+
+
+    handleEqualSign = () =>{
+        
     }
 
 
@@ -79,26 +213,26 @@ class Keypad extends Component {
                         <div className='func-rows'>
                             <p onClick={e => this.handleClearInput(1)}>AC</p>
                             <p onClick={e => this.handleClearInput(2)}>C</p>
-                            <p>%</p>
-                            <p className='four-or' style={{fontSize:'19px'}}>{'\u00F7'}</p>
+                            <p onClick={e => this.handleOperatorClear()}>OC</p>
+                            <p className='four-or' style={{fontSize:'19px'}} onClick={e => this.handleOperatorInput(1)}>{'\u00F7'}</p>
                         </div>
                         <div className='num-rows'>
                             <p onClick={e => this.handleNumberInput(7)}>7</p>
                             <p onClick={e => this.handleNumberInput(8)}>8</p>
                             <p onClick={e => this.handleNumberInput(9)}>9</p>
-                            <p className='four-or' style={{fontSize:'19px'}}>x</p>
+                            <p className='four-or' style={{fontSize:'19px'}} onClick={e => this.handleOperatorInput(2)}>x</p>
                         </div>
                         <div className='num-rows'>
                             <p onClick={e => this.handleNumberInput(4)}>4</p>
                             <p onClick={e => this.handleNumberInput(5)}>5</p>
                             <p onClick={e => this.handleNumberInput(6)}>6</p>
-                            <p className='four-or' style={{fontSize:'19px'}}>+</p>
+                            <p className='four-or' style={{fontSize:'19px'}} onClick={e => this.handleOperatorInput(3)}>+</p>
                         </div>
                         <div className='num-rows'>
                             <p onClick={e => this.handleNumberInput(1)}>1</p>
                             <p onClick={e => this.handleNumberInput(2)}>2</p>
                             <p onClick={e => this.handleNumberInput(3)}>3</p>
-                            <p className='four-or' style={{fontSize:'19px'}}>-</p>
+                            <p className='four-or' style={{fontSize:'19px'}} onClick={e => this.handleOperatorInput(4)}>-</p>
                         </div>
                         <div className='num-rows'>
                             <p id='double-size' onClick={e => this.handleNumberInput(0)}>0</p>
